48e6e111c2cf324b01e88f3d638b727d22fac38f
lyabomyr <lyabomyr@gmail.com>
2022-12-04 02:49:31 +0200

    add first interration with pdf file

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from skills import skills
 from experience import experience
 from education_and_cources import education
 
-def main():
+def structure():
     st.markdown(""" <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
@@ -14,16 +14,16 @@ def main():
     experience()
     education()
 
-with st.sidebar:  
-    with open("src/CV_QA_Kachko_Luibomyr.pdf", "rb") as pdf_file:
-        PDFbyte = pdf_file.read()
+def main():
+    structure()
+    with st.sidebar:  
+        with open("src/CV_QA_Kachko_Luibomyr.pdf", "rb") as pdf_file:
+            PDFbyte = pdf_file.read()
 
-    st.download_button(label=" Download CV_QA_Kachko_Luibomyr.pdf",
-                    data=PDFbyte,
-                    file_name="CV_QA_Kachko_Luibomyr.pdf",
-                    mime='application/octet-stream')
-main()
+        st.download_button(label=" Download CV_QA_Kachko_Luibomyr.pdf",
+                        data=PDFbyte,
+                        file_name="CV_QA_Kachko_Luibomyr.pdf",
+                        mime='application/octet-stream')
 
-
-
-
+if __name__ == "__main__":
+    main()
